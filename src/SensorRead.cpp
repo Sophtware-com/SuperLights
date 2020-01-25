2434a5b92ce8b017907ebf88be5b9a7b4dd56f78
Michael Lange <michael@Michaels-MacBook-Pro.local>
2020-01-25 15:34:15 -0500

    Fixed knob directions. Left Less, RIght More.

--- src/SensorRead.cpp
+++ src/SensorRead.cpp
@@ -1,5 +1,5 @@
 #include "SensorRead.h"
 
-SensorRead _bright(BRIGHT_VAL_PIN, 3);
-SensorRead _speed(SPEED_VAL_PIN, 3, 2, true);
-SensorRead _color(COLOR_VAL_PIN, 3);
+SensorRead _bright(BRIGHT_VAL_PIN, 3, 2, true);
+SensorRead _speed(SPEED_VAL_PIN, 3);
+SensorRead _color(COLOR_VAL_PIN, 3, 2, true);
